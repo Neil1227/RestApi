dd57f94c1128a1306c2c731721e7f572d5d91353
Neil Patrick Acierto <neilpatrickacierto27@gmail.com>
2025-04-21 13:31:13 +0800

    Sign in from the offics

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,7 +38,7 @@
                 </span>
             @enderror
         </div>
-        <button type="submit" class="btn btn-login btn-block">LOGIN</button>
+        <button type="submit" class="btn btn-login btn-block">Sign In</button>
     </form>
 </div>
 
